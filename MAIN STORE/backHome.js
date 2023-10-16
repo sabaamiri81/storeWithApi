798d913fe02d765bcb2d1d5576deca2c2e1f1393
Saba Amiri <amirisaba043@gmail.com>
2023-10-16 16:00:25 +0330

    remove spaces

--- MAIN STORE/backHome.js
+++ MAIN STORE/backHome.js
@@ -1,18 +1,11 @@
 
-function backHome(){
+function backHome() {
     firstHomeCards__btn.classList.remove("firstHomeCards__btnDisplay");
     secondHomeCards.classList.remove("secondHomeCardsDisplay");
     secondSlide.classList.remove("secondSlideDisplay");
     slide.classList.remove("slideDisplay");
+    root.classList.add("rootDisplay");
 }
 
 
-
-
-
-
-
-
-
-
-homeIcon.addEventListener("click" , backHome)
+homeIcon.addEventListener("click", backHome)
